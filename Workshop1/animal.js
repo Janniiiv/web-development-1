809690117c07bbef7bf800fd7aa74ce74a4d62a5
Janni Virta <janni.virta@hotmail.com>
2026-09-22 14:43:27 +0300

    commit

--- Workshop1/animal.js
+++ Workshop1/animal.js
@@ -16,3 +16,34 @@ animalImage.addEventListener("mouseover", function() {
     animalImage.style.border = "10px solid black";
 });
 
+//input event
+
+const animalInput = document.querySelector("#animalInput");
+const animalOutput = document.querySelector("#animalOutput");
+
+animalInput.addEventListener("input", function() {
+    animalOutput.textContent = animalInput.value;
+});
+
+animalInput.addEventListener("focus", function() {
+    console.log("input is focused");
+});
+
+animalInput.addEventListener("blur", function() {
+    console.log("input lost focus");
+});
+
+
+const animalForm = document.querySelector("#animalForm");
+    animalForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        animalOutput.textContent = "Your favorite animal is " + animalInput.value;
+        console.log("Form submitted with the animal: " + animalInput.value);
+
+    });
+
+document.addEventListener("keydown", function(event) {
+    console.log("A key was pressed:" + event.key);
+    console.log("Key code:" + event.code);
+});
+
