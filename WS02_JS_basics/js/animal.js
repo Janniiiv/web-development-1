4fecbfb3f40cf93f6f4ccd40946dbd9681cc75d6
Janni Virta <janni.virta@hotmail.com>
2026-09-22 08:33:19 +0300

    commit

---
+++ WS02_JS_basics/js/animal.js
@@ -0,0 +1,5 @@
+//when user clicks button show message about puppy.
+const animalbutton = document.getElementById("animalbutton");
+animalbutton. addEventListener("click", function() {
+    alert("Puppies are cute!");
+});
